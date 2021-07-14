3531f88e71770a1e51b45863995ec938d6e498c5
ahmednassereglan <ahmednassereglan@gmail.com>
2021-07-14 18:41:53 +0200

    Add project files.

---
+++ Core/Entities/PortfolioItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities
+{
+    public class PortfolioItem : EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Descraption { get; set; }
+        public string ImageUrl { get; set; }
+        
+
+    }
+}
